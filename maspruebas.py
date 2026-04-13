ed313905e7b38dbaa55f7ca698616aa0fd834b20
jorgecabrera-dev <jorge.cabrera.llanos@gmail.com>
2026-04-13 10:10:55 -0400

    segundo commit

--- maspruebas.py
+++ maspruebas.py
@@ -8,19 +8,6 @@ if edad >= 18:
 else:
     print ('Es menor de edad')
 
-num1 = int(input('Ingrese un número: '))
-num2 = int(input('Ingrese otro número: '))
-
-suma = num1 + num2
-resta = num1 - num2
-multi = num1 * num2
-div = num1 / num2
-
-print('El resultado de la suma de ambos números es: ', suma)
-print('El resultado de la resta de ambos números es: ', resta)
-print('El resultado de la multiplicación de ambos números es: ', multi)
-print('El resultado de la división de ambos números es: ', div)
-
 afir = input('¿Hoy está lloviendo?: (si/no)')
 
 if afir == 'si':
